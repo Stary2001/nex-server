45a1d6f5c663b3846724ea8030b9f82ded0682a7
Joshua Jones <shutterbug2000@fs3d.net>
2017-10-31 21:49:53 +0000

    Now properly handles the first 2 packets

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import asyncio
 from prudp import PRUDPProtocol
 
 loop = asyncio.get_event_loop()
-listen = loop.create_datagram_endpoint(PRUDPProtocol, local_addr=('0.0.0.0', 60000))
+listen = loop.create_datagram_endpoint(PRUDPProtocol, local_addr=('0.0.0.0', 9999))
 transport, protocol = loop.run_until_complete(listen)
 
 try:
--- prudp/__init__.py
+++ prudp/__init__.py
@@ -7,15 +7,17 @@ class PRUDPV0Packet:
     FLAG_NEEDS_ACK = 0x4
     FLAG_HAS_SIZE = 0x8
 
-    def __init__(self, direction, op, flags, seq, conn_sig):
+    def __init__(self, direction, op, flags, seq, conn_sig, session, sig):
         self.direction = direction
         self.op = op
         self.flags = flags
         self.seq = seq
         self.conn_sig = conn_sig
+        self.session = session
+        self.sig = sig
 
     @classmethod
-    def decode(data, rc4_state):
+    def decode(self, data, rc4_state):
         # Decode the packet header..
         direction = struct.unpack("<H", data[0:2])[0]
 
@@ -30,7 +32,7 @@ class PRUDPV0Packet:
 
         print("direction={:04x}, op_flags={:04x}, session={:02x}, sig={:08x}, seq={:02x}, conn_sig={:08x}".format(direction, op_flags, session, sig, seq, conn_sig))
 
-        return PRUDPV0Packet(direction=direction, op=op, flags=flags, seq=seq, conn_sig=conn_sig)
+        return PRUDPV0Packet(direction=direction, op=op, flags=flags, seq=seq, conn_sig=conn_sig, session=session, sig=sig)
 
     def encode(self, rc4_state):
         pass
@@ -46,13 +48,32 @@ class PRUDPClient:
 
         state = PRUDPClient.STATE_EXPECT_SYN
 
+    def calc_checksum(self, checksum,data):
+        words = struct.unpack_from("<"+"I"*(len(data)//4),data)
+        temp = sum(words) & 0xffffffff
+
+        checksum+=sum(data[-(len(data) & 3):])
+        checksum+=sum(struct.pack("I", temp))
+
+        return checksum & 0xff
+
+
     def decode_packet(self, data):
-        return PRUDPV0Packet.decode()
+        return PRUDPV0Packet.decode(data,self.rc4_state_decrypt)
 
     def encode_packet(self):
         # ...
         # return a bytestring
-        pass
+        data=b""
+        data+=struct.pack("BB",self.source,self.dest)
+        data+=struct.pack("<H",self.op | self.flags)
+        data+=struct.pack("B",self.session)
+        data+=struct.pack("<I",self.sig)
+        data+=struct.pack("<H",self.seq)
+        data+=struct.pack("<I",0)
+        data+=struct.pack("<H",0)
+        data+=struct.pack("B",self.calc_checksum(sum("ridfebb9".encode("ascii")),data))
+        return data
 
     def handle_data(self, data):
         return self.handle_packet(self.decode_packet(data))
@@ -60,7 +81,16 @@ class PRUDPClient:
     def handle_packet(self, packet):
         # ...
         # do something here?
-        pass
+        if True:
+            self.source = 0xa1
+            self.dest = 0xaf
+            self.op = packet.op
+            self.flags = 0x90
+            self.sig = packet.conn_sig
+            self.session = packet.session
+            self.seq = packet.seq
+            self.size = 0
+            return self.encode_packet()
 
 # TODO: big issue with this is stray UDP packets.
 # Time out connections after <some time> of lingering in STATE_EXPECT_SYN.
@@ -70,6 +100,9 @@ class PRUDPProtocol(asyncio.DatagramProtocol):
         super().__init__()
         self.connections = {}
 
+    def connection_made(self, transport):
+        self.transport = transport
+
     def datagram_received(self, data, addr):
         if not addr in self.connections:
             client = PRUDPClient(b"CD&ML")
@@ -77,4 +110,5 @@ class PRUDPProtocol(asyncio.DatagramProtocol):
         else:
             client = self.connections[addr]
 
-        client.handle_data(data)
+        packet = client.handle_data(data)
+        self.transport.sendto(packet, addr)
