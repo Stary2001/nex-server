cb8dd0d5c0ba6a714857264a98cd9aef17c4bcd5
Ezekiel Bethel <mctinfoilball@gmail.com>
2017-10-31 21:28:24 +0000

    packet decode? maybe?

--- prudp/__init__.py
+++ prudp/__init__.py
@@ -1,8 +1,38 @@
 import asyncio
+import struct
 from rc4 import RC4
 
 class PRUDPV0Packet:
-    def decode(self, rc4_state):
+    FLAG_RELIABLE = 0x2
+    FLAG_NEEDS_ACK = 0x4
+    FLAG_HAS_SIZE = 0x8
+
+    def __init__(self, direction, op, flags, seq, conn_sig):
+        self.direction = direction
+        self.op = op
+        self.flags = flags
+        self.seq = seq
+        self.conn_sig = conn_sig
+
+    @classmethod
+    def decode(data, rc4_state):
+        # Decode the packet header..
+        direction = struct.unpack("<H", data[0:2])[0]
+
+        op_flags = struct.unpack("<H", data[2:4])[0]
+        op = op_flags & 0xF
+        flags = (op_flags & 0xFFF0) >> 4
+
+        session = data[4]
+        sig = struct.unpack("<I", data[5:9])[0]
+        seq = struct.unpack("<H", data[9:11])[0]
+        conn_sig = struct.unpack("<I", data[11:15])[0]
+
+        print("direction={:04x}, op_flags={:04x}, session={:02x}, sig={:08x}, seq={:02x}, conn_sig={:08x}".format(direction, op_flags, session, sig, seq, conn_sig))
+
+        return PRUDPV0Packet(direction=direction, op=op, flags=flags, seq=seq, conn_sig=conn_sig)
+
+    def encode(self, rc4_state):
         pass
 
 class PRUDPClient:
@@ -17,9 +47,7 @@ class PRUDPClient:
         state = PRUDPClient.STATE_EXPECT_SYN
 
     def decode_packet(self, data):
-        # ...
-        # return a PRUDPV0Packet
-        pass
+        return PRUDPV0Packet.decode()
 
     def encode_packet(self):
         # ...
